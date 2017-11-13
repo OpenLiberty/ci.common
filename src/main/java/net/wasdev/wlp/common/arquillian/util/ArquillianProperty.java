8ce7ee5b2c448722e5fd9c0202790d4c4f04191d
Charles Tian <ctian@us.ibm.com>
2017-11-13 15:19:52 -0600

    Build the arquillian.xml automatically

---
+++ src/main/java/net/wasdev/wlp/common/arquillian/util/ArquillianProperty.java
@@ -0,0 +1,10 @@
+package net.wasdev.wlp.common.arquillian.util;
+
+public enum ArquillianProperty {
+	verifiedApps,
+	appDeployTimeout,
+	verifiedAppDeployTimeout,
+	appUndeployTimeout,
+	outputToConsole,
+	allowConnectingToRunningServer
+}
