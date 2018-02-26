a890d9ef689e6ea5b8cf08e380de8fcc3ff030d0
Anjum Fatima <anjum.fatima@ibm.com>
2018-02-26 10:01:25 -0600

    Added SpringBootConstants class

---
+++ src/main/java/com/ibm/ws/app/manager/springboot/internal/SpringConstants.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.app.manager.springboot.internal;
+
+public class SpringConstants {
+	public static final String SPRING_START_CLASS_HEADER = "Start-Class";
+    public static final String SPRING_BOOT_CLASSES_HEADER = "Spring-Boot-Classes";
+    public static final String SPRING_BOOT_LIB_HEADER = "Spring-Boot-Lib";
+    public static final String SPRING_LIB_INDEX_FILE = "META-INF/spring.lib.index";
+
+}
