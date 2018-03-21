c51681ab46efaf41b07f8f79af634cae2598eb0b
Anjum Fatima <anjum.fatima@ibm.com>
2018-03-21 18:19:17 -0500

    Change package name

--- src/main/java/net/wasdev/wlp/common/springboot/util/SpringBootThinUtil.java
+++ src/main/java/net/wasdev/wlp/common/springboot/util/SpringBootThinUtil.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package com.ibm.ws.app.manager.springboot.util;
+package net.wasdev.wlp.common.springboot.util;
 
 import java.io.File;
 import java.io.FileNotFoundException;
