3bcd78653d34e3226261e234f38d72a3b0bf8cd5
Anjum Fatima <anjum.fatima@ibm.com>
2018-07-12 13:27:59 -0500

    Update to latest changes in SpringBootThinUtil and SpringBootManifest as per open-liberty

--- src/main/java/net/wasdev/wlp/common/springboot/util/SpringBootManifest.java
+++ src/main/java/net/wasdev/wlp/common/springboot/util/SpringBootManifest.java
@@ -1,13 +1,18 @@
-/*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/**
+ * (C) Copyright IBM Corporation 2018.
  *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.wasdev.wlp.common.springboot.util;
 
 import java.util.jar.Manifest;
@@ -24,22 +29,29 @@ public class SpringBootManifest {
 
     enum SpringLauncher {
         JarLauncher("JarLauncher", "BOOT-INF/lib/", "BOOT-INF/classes/"),
-        WarLauncher("WarLauncher", "WEB-INF/lib/", "WEB-INF/classes/");
-    	private String name;
-        private String libDefault;
-        private String classesDefault;
+        WarLauncher("WarLauncher", "WEB-INF/lib/", "WEB-INF/classes/", "-provided");
 
         private SpringLauncher(String name, String libDefault, String classesDefault) {
+            this(name, libDefault, classesDefault, null);
+        }
+
+        private SpringLauncher(String name, String libDefault, String classesDefault, String libProvidedSuffix) {
             this.name = name;
             this.libDefault = libDefault;
             this.classesDefault = classesDefault;
+            this.libProvidedSuffix = libProvidedSuffix;
         }
+
+        private final String name;
+        private final String libDefault;
+        private final String classesDefault;
+        private final String libProvidedSuffix;
 
         static SpringLauncher fromMainClass(String mainClass) {
             if (mainClass != null) {
-                String mClass = mainClass.trim();
+                mainClass = mainClass.trim();
                 for (SpringLauncher l : SpringLauncher.values()) {
-                    if (mClass.endsWith(l.name)) {
+                    if (mainClass.endsWith(l.name)) {
                         return l;
                     }
                 }
@@ -57,11 +69,16 @@ public class SpringBootManifest {
                     return null;
             }
         }
+
+        String getLibProvidedSuffix() {
+            return libProvidedSuffix;
+        }
     }
 
     private final String springStartClass;
     private final String springBootClasses;
     private final String springBootLib;
+    private final String springBootLibPrivided;
 
     /**
      * Returns the start class for the Spring Boot application.
@@ -91,12 +108,23 @@ public class SpringBootManifest {
         return springBootLib;
     }
 
+    /**
+     * Returns the path to the lib provided folder for the Spring Boot
+     * application
+     *
+     * @return the path to the lib provided folder
+     */
+    public String getSpringBootLibProvided() {
+        return springBootLibPrivided;
+    }
+
     public SpringBootManifest(Manifest mf) {
         String mainClass = mf.getMainAttributes().getValue(JAR_MAIN_CLASS);
         SpringLauncher launcher = SpringLauncher.fromMainClass(mainClass);
         springStartClass = mf.getMainAttributes().getValue(SPRING_START_CLASS_HEADER);
         springBootClasses = getSpringHeader(mf, SPRING_BOOT_CLASSES_HEADER, launcher);
         springBootLib = getSpringHeader(mf, SPRING_BOOT_LIB_HEADER, launcher);
+        springBootLibPrivided = getLibProvided(launcher, springBootLib);
     }
 
     private static String getSpringHeader(Manifest mf, String springBootHeaderKey, SpringLauncher launcher) {
@@ -112,5 +140,13 @@ public class SpringBootManifest {
             return path.substring(0, path.length() - 1);
         }
         return path;
+    }
+
+    private static String getLibProvided(SpringLauncher launcher, String springBootLib) {
+        String suffix = launcher.getLibProvidedSuffix();
+        if (suffix != null) {
+            return springBootLib + suffix;
+        }
+        return null;
     }
 }
