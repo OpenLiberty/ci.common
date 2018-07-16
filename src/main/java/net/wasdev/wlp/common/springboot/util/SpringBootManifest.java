35212a6257b340c2cccf0560fe9a9c9653e3715b
Anjum Fatima <anjum.fatima@ibm.com>
2018-07-16 15:24:33 -0500

    Formatted the code

--- src/main/java/net/wasdev/wlp/common/springboot/util/SpringBootManifest.java
+++ src/main/java/net/wasdev/wlp/common/springboot/util/SpringBootManifest.java
@@ -18,128 +18,125 @@ package net.wasdev.wlp.common.springboot.util;
 import java.util.jar.Manifest;
 
 /**
- * Relevant Spring Boot information contained in the Spring Boot JAR
- * manifest.
+ * Relevant Spring Boot information contained in the Spring Boot JAR manifest.
  */
 public class SpringBootManifest {
-    private static final String JAR_MAIN_CLASS = "Main-Class";
-    private static final String SPRING_START_CLASS_HEADER = "Start-Class";
-    private static final String SPRING_BOOT_CLASSES_HEADER = "Spring-Boot-Classes";
-    private static final String SPRING_BOOT_LIB_HEADER = "Spring-Boot-Lib";
+	private static final String JAR_MAIN_CLASS = "Main-Class";
+	private static final String SPRING_START_CLASS_HEADER = "Start-Class";
+	private static final String SPRING_BOOT_CLASSES_HEADER = "Spring-Boot-Classes";
+	private static final String SPRING_BOOT_LIB_HEADER = "Spring-Boot-Lib";
 
-    enum SpringLauncher {
-        JarLauncher("JarLauncher", "BOOT-INF/lib/", "BOOT-INF/classes/"),
-        WarLauncher("WarLauncher", "WEB-INF/lib/", "WEB-INF/classes/", "-provided");
+	enum SpringLauncher {
+		JarLauncher("JarLauncher", "BOOT-INF/lib/", "BOOT-INF/classes/"), WarLauncher("WarLauncher", "WEB-INF/lib/",
+				"WEB-INF/classes/", "-provided");
 
-        private SpringLauncher(String name, String libDefault, String classesDefault) {
-            this(name, libDefault, classesDefault, null);
-        }
+		private SpringLauncher(String name, String libDefault, String classesDefault) {
+			this(name, libDefault, classesDefault, null);
+		}
 
-        private SpringLauncher(String name, String libDefault, String classesDefault, String libProvidedSuffix) {
-            this.name = name;
-            this.libDefault = libDefault;
-            this.classesDefault = classesDefault;
-            this.libProvidedSuffix = libProvidedSuffix;
-        }
+		private SpringLauncher(String name, String libDefault, String classesDefault, String libProvidedSuffix) {
+			this.name = name;
+			this.libDefault = libDefault;
+			this.classesDefault = classesDefault;
+			this.libProvidedSuffix = libProvidedSuffix;
+		}
 
-        private final String name;
-        private final String libDefault;
-        private final String classesDefault;
-        private final String libProvidedSuffix;
+		private final String name;
+		private final String libDefault;
+		private final String classesDefault;
+		private final String libProvidedSuffix;
 
-        static SpringLauncher fromMainClass(String mainClass) {
-            if (mainClass != null) {
-                mainClass = mainClass.trim();
-                for (SpringLauncher l : SpringLauncher.values()) {
-                    if (mainClass.endsWith(l.name)) {
-                        return l;
-                    }
-                }
-            }
-            return JarLauncher;
-        }
+		static SpringLauncher fromMainClass(String mainClass) {
+			if (mainClass != null) {
+				mainClass = mainClass.trim();
+				for (SpringLauncher l : SpringLauncher.values()) {
+					if (mainClass.endsWith(l.name)) {
+						return l;
+					}
+				}
+			}
+			return JarLauncher;
+		}
 
-        String getDefault(String springBootHeaderKey) {
-            switch (springBootHeaderKey) {
-                case SPRING_BOOT_CLASSES_HEADER:
-                    return classesDefault;
-                case SPRING_BOOT_LIB_HEADER:
-                    return libDefault;
-                default:
-                    return null;
-            }
-        }
+		String getDefault(String springBootHeaderKey) {
+			switch (springBootHeaderKey) {
+			case SPRING_BOOT_CLASSES_HEADER:
+				return classesDefault;
+			case SPRING_BOOT_LIB_HEADER:
+				return libDefault;
+			default:
+				return null;
+			}
+		}
 
-        String getLibProvidedSuffix() {
-            return libProvidedSuffix;
-        }
-    }
+		String getLibProvidedSuffix() {
+			return libProvidedSuffix;
+		}
+	}
 
-    private final String springStartClass;
-    private final String springBootClasses;
-    private final String springBootLib;
-    private final String springBootLibPrivided;
+	private final String springStartClass;
+	private final String springBootClasses;
+	private final String springBootLib;
+	private final String springBootLibPrivided;
 
-    /**
-     * Returns the start class for the Spring Boot application.
-     *
-     * @return the start class
-     */
-    public String getSpringStartClass() {
-        return springStartClass;
-    }
+	/**
+	 * Returns the start class for the Spring Boot application.
+	 *
+	 * @return the start class
+	 */
+	public String getSpringStartClass() {
+		return springStartClass;
+	}
 
-    /**
-     * Returns the path to the classes directory for the Spring
-     * Boot Application
-     *
-     * @return the path to the classes directory
-     */
-    public String getSpringBootClasses() {
-        return springBootClasses;
-    }
+	/**
+	 * Returns the path to the classes directory for the Spring Boot Application
+	 *
+	 * @return the path to the classes directory
+	 */
+	public String getSpringBootClasses() {
+		return springBootClasses;
+	}
 
-    /**
-     * Returns the path to the lib folder for the Spring Boot application
-     *
-     * @return the path to the lib folder
-     */
-    public String getSpringBootLib() {
-        return springBootLib;
-    }
+	/**
+	 * Returns the path to the lib folder for the Spring Boot application
+	 *
+	 * @return the path to the lib folder
+	 */
+	public String getSpringBootLib() {
+		return springBootLib;
+	}
 
-    /**
-     * Returns the path to the lib provided folder for the Spring Boot
-     * application
-     *
-     * @return the path to the lib provided folder
-     */
-    public String getSpringBootLibProvided() {
-        return springBootLibPrivided;
-    }
+	/**
+	 * Returns the path to the lib provided folder for the Spring Boot application
+	 *
+	 * @return the path to the lib provided folder
+	 */
+	public String getSpringBootLibProvided() {
+		return springBootLibPrivided;
+	}
 
-    public SpringBootManifest(Manifest mf) {
-        String mainClass = mf.getMainAttributes().getValue(JAR_MAIN_CLASS);
-        SpringLauncher launcher = SpringLauncher.fromMainClass(mainClass);
-        springStartClass = mf.getMainAttributes().getValue(SPRING_START_CLASS_HEADER);
-        springBootClasses = getSpringHeader(mf, SPRING_BOOT_CLASSES_HEADER, launcher);
-        springBootLib = getSpringHeader(mf, SPRING_BOOT_LIB_HEADER, launcher);
-        springBootLibPrivided = getLibProvided(launcher, springBootLib);
-    }
+	public SpringBootManifest(Manifest mf) {
+		String mainClass = mf.getMainAttributes().getValue(JAR_MAIN_CLASS);
+		SpringLauncher launcher = SpringLauncher.fromMainClass(mainClass);
+		springStartClass = mf.getMainAttributes().getValue(SPRING_START_CLASS_HEADER);
+		springBootClasses = getSpringHeader(mf, SPRING_BOOT_CLASSES_HEADER, launcher);
+		springBootLib = getSpringHeader(mf, SPRING_BOOT_LIB_HEADER, launcher);
+		springBootLibPrivided = getLibProvided(launcher, springBootLib);
+	}
 
-    private static String getSpringHeader(Manifest mf, String springBootHeaderKey, SpringLauncher launcher) {
-        String value = mf.getMainAttributes().getValue(springBootHeaderKey);
-        if (value == null) {
-            value = launcher.getDefault(springBootHeaderKey);
-        }
-        return value;
-    }
+	private static String getSpringHeader(Manifest mf, String springBootHeaderKey, SpringLauncher launcher) {
+		String value = mf.getMainAttributes().getValue(springBootHeaderKey);
+		if (value == null) {
+			value = launcher.getDefault(springBootHeaderKey);
+		}
+		return value;
+	}
 
-    private static String getLibProvided(SpringLauncher launcher, String springBootLib) {
-        String suffix = launcher.getLibProvidedSuffix();
-        if (suffix != null) {
-            return springBootLib + suffix;
-        }
-        return null;
-    }
+	private static String getLibProvided(SpringLauncher launcher, String springBootLib) {
+		String suffix = launcher.getLibProvidedSuffix();
+		if (suffix != null) {
+			return springBootLib + suffix;
+		}
+		return null;
+	}
 }
