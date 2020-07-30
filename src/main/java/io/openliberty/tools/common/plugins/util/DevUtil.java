a1c93fac06863bda0cebac2f0dc8c1ab27433cc8
Turkeylurkey <turkeyonmarblerye@gmail.com>
2020-07-30 14:48:43 -0400

    Include docker build error message in exception to display in final build message.

--- src/main/java/io/openliberty/tools/common/plugins/util/DevUtil.java
+++ src/main/java/io/openliberty/tools/common/plugins/util/DevUtil.java
@@ -938,7 +938,7 @@ public abstract class DevUtil {
             debug("Docker build output: " + buildOutput);
         } catch (RuntimeException r) {
             error("Error building Docker image: " + r.getMessage());
-            throw new PluginExecutionException("Could not build Docker image using Dockerfile: " + userDockerfile.getAbsolutePath() + ". Address the following docker build error and then start dev mode again", r);
+            throw new PluginExecutionException("Could not build Docker image using Dockerfile: " + userDockerfile.getAbsolutePath() + ". Address the following docker build error and then start dev mode again:"+r.getMessage(), r);
         }
     }
 
