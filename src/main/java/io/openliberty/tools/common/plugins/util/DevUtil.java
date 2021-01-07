5f995aae2bf01e8eeaa98c8669fcfb56a80853dc
Turkeylurkey <turkeyonmarblerye@gmail.com>
2021-01-07 16:26:50 -0500

    Log RUN features.sh message as info since warn is too strong for a normal command.

--- src/main/java/io/openliberty/tools/common/plugins/util/DevUtil.java
+++ src/main/java/io/openliberty/tools/common/plugins/util/DevUtil.java
@@ -1101,7 +1101,7 @@ public abstract class DevUtil extends AbstractContainerSupportUtil {
             String buildCmd = sb.toString();
             info(buildCmd);
             if (hasFeaturesSh.get()) {
-                warn("The RUN features.sh command is detected in the Dockerfile and extra time may be necessary when installing features.");
+                info("The RUN features.sh command is detected in the Dockerfile and extra time may be necessary when installing features.");
             }
             long startTime = System.currentTimeMillis();
             execDockerCmdAndLog(getRunProcess(buildCmd), dockerBuildTimeout);
@@ -1142,8 +1142,8 @@ public abstract class DevUtil extends AbstractContainerSupportUtil {
             } catch (IOException e) {
                 buildContextPath = dockerBuildContext.getAbsolutePath();
             }
-            warn("The docker build command took longer than " + DOCKER_BUILD_SOFT_TIMEOUT / 1000 + " seconds." +
-                " You may increase performance by adding unneeded files and directories " +
+            warn("The docker build command took longer than " + DOCKER_BUILD_SOFT_TIMEOUT / 1000 + " seconds. " +
+                "You may increase performance by adding unneeded files and directories " +
                 "such as any Liberty runtime directories to a .dockerignore file in " +
                 buildContextPath + ".");
         }
