bd86b9125a262fc8d1902adec23f8e04558f80e3
Paul Gooderham <turkeyonmarblerye@gmail.com>
2025-06-17 19:26:55 -0400

    Keep temp generate features file in configDropins dir even in temp dir
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

--- src/main/java/io/openliberty/tools/common/plugins/util/BinaryScannerUtil.java
+++ src/main/java/io/openliberty/tools/common/plugins/util/BinaryScannerUtil.java
@@ -33,9 +33,10 @@ public abstract class BinaryScannerUtil {
     public static final String BINARY_SCANNER_MAVEN_VERSION = "[24.0.0.2]";
 
     public static final String GENERATED_FEATURES_FILE_NAME = "generated-features.xml";
-    public static final String GENERATED_FEATURES_DIR_PATH = "configDropins/overrides";
-    public static final String GENERATED_FEATURES_FILE_PATH = GENERATED_FEATURES_DIR_PATH + File.pathSeparator + GENERATED_FEATURES_FILE_NAME;
-    public static final String GENERATED_FEATURES_TEMP_PATH = ".libertyFeatures/" + GENERATED_FEATURES_FILE_NAME;
+    public static final String GENERATED_FEATURES_DIR_PATH = "configDropins/overrides/";
+    public static final String GENERATED_FEATURES_FILE_PATH = GENERATED_FEATURES_DIR_PATH + GENERATED_FEATURES_FILE_NAME;
+    public static final String GENERATED_FEATURES_TEMP_DIR = ".libertyFeatures";
+    public static final String GENERATED_FEATURES_TEMP_PATH = GENERATED_FEATURES_TEMP_DIR + File.separator + GENERATED_FEATURES_FILE_PATH;
     private static final String FEATURE_MODIFIED_EXCEPTION = "com.ibm.websphere.binary.cmdline.exceptions.RequiredFeatureModifiedException";
     private static final String FEATURE_CONFLICT_EXCEPTION = "com.ibm.websphere.binary.cmdline.exceptions.FeatureConflictException";
     private static final String PROVIDED_FEATURE_EXCEPTION = "com.ibm.websphere.binary.cmdline.exceptions.ProvidedFeatureConflictException";
